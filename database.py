d47138a643a358044b54b29fa439a7b9ef8f350e
Арсений <115050782+ArPaSokolov@users.noreply.github.com>
2023-06-27 13:32:48 +0200

    Запись в файл, проверка на уникальность и сортировка

--- database.py
+++ database.py
@@ -1,3 +1,20 @@
-with open('database.txt', 'a') as f:
-    # Добавляем строку в файл
-    f.write("Новая строка\n")
+# Открываем файл для чтения
+with open("database.txt", "r") as file:
+    # Читаем все строки из файла и сохраняем уникальные имена в множество
+    users = set(file.read().splitlines())
+
+# Выводим имена пользователей на экран
+for user in users:
+    print(user)
+
+# Открываем файл для записи
+with open("database.txt", "a") as file:
+    # Запрашиваем у пользователя новое имя и добавляем его в множество
+    new_user = input("Введите новое имя пользователя: ")
+    users.add(new_user)
+
+    # Записываем все уникальные имена пользователей из множества в файл
+    file.seek(0)
+    file.truncate()
+    for user in users:
+        file.write(user + "\n")
