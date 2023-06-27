69fea027e443fa5378f32ba53085335c9389d796
Alex <a.a.sagitovich@gmail.com>
2023-06-27 10:01:13 +0200

    upd parser.py (27.06 10:01)

--- parser.py
+++ parser.py
@@ -2,45 +2,45 @@ import requests
 from bs4 import BeautifulSoup
 
 user_id = input("Введите id пользователя: ")
+url = "https://vk.com/" + user_id              # создание ссылки
+req = requests.get(url)                        # получаем html код страницы пользователя
+src = req.text                                 # переводим его в сплошной текст
 
-url = "https://vk.com/" + user_id
-req = requests.get(url)
-src = req.text
+with open("index.html", "w") as file:           # открываем файл "index.html" для записи (он автоматически создаётся)
+    file.write(src)                             # записываем в него html код страницы пользователя
 
-with open("index.html", "w") as file:
-    file.write(src)
+with open("index.html") as file:                # открываем файл "index.html"
+    src = file.read()                           # считываем его в переменную src
 
-with open("index.html") as file:
-    src = file.read()
+soup = BeautifulSoup(src, "lxml")              # создаём переменную, вызывая которую, можно собирать данные с файла
 
-soup = BeautifulSoup(src, "lxml")
-
-print("Ссылка на страницу: ", f"{url}")        # ССЫЛКА НА СТРАНИЦУ
-
-user_name = soup.find(class_="op_header")       # ИМЯ
-for i in user_name:
-    name = i.text
+user_name = soup.find(class_="op_header")       # получаем имя пользователя (получим список)
+for i in user_name:                            # пройдёмся по каждому объекту полученного списка
+    name = i.text                              # избавимся от технических символов
     print("Имя пользователя: ", f"{name.strip()}")
 
-# last_activity = soup.find(class_="pp_last_activity_text")       # ПОСЛЕДНЯЯ АКТИВНОСТЬ
-# for i in last_activity:
-#     activity = i.text
-#     print("Последняя активность: ", f"{activity.strip()}")
-
-# user_birthday = soup.find(class_="")                            # ДАТА РОЖДЕНИЯ
-# for i in user_birthday:
-#     birthday = i.text
-#     print("Дата рождения: ", f"{birthday.strip()}")
+last_activity = soup.find(class_="pp_last_activity_text")       # получаем последнюю активность пользователя
+for i in last_activity:
+    activity = i.text
+    print('Последняя активность: ', f"{activity.strip()}")
 
 try:
     a = []
     cnt = 0
-    about_user = soup.find_all(class_="OwnerInfo__rowCenter")    # ПОДРОБНАЯ ИНФА
+    about_user = soup.find_all(class_="OwnerInfo__rowCenter")    # получаем подробную информацию о пользователе
     for i in about_user:
         about = i.text
         a.append(about)
         cnt += 1
     for i in range(0, cnt - 1):
         print(f"{a[i].strip()}")
-except:
+except NameError:
     print("Страница пользователя является закрытой")
+
+print("Ссылка на страницу: ", f"{url}")        # ССЫЛКА НА СТРАНИЦУ
+
+
+# user_birthday = soup.find(class_="")                            # ДАТА РОЖДЕНИЯ
+# for i in user_birthday:
+#     birthday = i.text
+#     print("Дата рождения: ", f"{birthday.strip()}")
