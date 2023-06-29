08965394e2a78cf41f0906dcd056a8eeedf84120
Gvozdem <gvozdenko.demid@mail.ru>
2023-06-29 14:06:10 +0200

    Поменял немного расположение команд,так будет правильней (ничего не появилось)

--- Bot.py
+++ Bot.py
@@ -14,19 +14,6 @@ def startBot(message):
     bot.send_message(message.chat.id, first_mess, parse_mode='html', reply_markup=markup)  # Вывод всего что выше
 
 
-@bot.callback_query_handler(func=lambda call: True)
-def response(function_call):
-    if function_call.message:
-        if function_call.data == "yes":  # Если нажали кнопку "Да"
-            second_mess = "Напиши id человека, которого хочешь найти"  # Второе сообщение, после "Да"
-            bot.send_message(function_call.message.chat.id, second_mess)  # Показать второе сообщение
-            bot.answer_callback_query(function_call.id)  # Обработка команды закончена
-        elif function_call.data == "end":
-            end_mess = "Приятно было познакомится, пока!"
-            bot.send_message(function_call.message.chat.id, end_mess)
-            bot.answer_callback_query(function_call.id)
-
-
 @bot.message_handler(commands=['help'])  # Команда хелп
 def helpBot(message):
     help_mess = f"Вот команды, которые я умею:\n/start (Начало работы)\n/stop (Конец работы)\nТакже в мои возможности входит поиск людей во ВКонтакте.\nЧтобы найти человека напиши его id в VK"
@@ -41,6 +28,19 @@ def stopBot(message):
     button_no = types.InlineKeyboardButton(text='Нет', callback_data='yes')
     markup.add(button_yes, button_no)
     bot.send_message(message.chat.id, stop_mess, parse_mode='html', reply_markup=markup)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def response(function_call):
+    if function_call.message:
+        if function_call.data == "yes":  # Если нажали кнопку "Да"
+            second_mess = "Напиши id человека, которого хочешь найти"  # Второе сообщение, после "Да"
+            bot.send_message(function_call.message.chat.id, second_mess)  # Показать второе сообщение
+            bot.answer_callback_query(function_call.id)  # Обработка команды закончена
+        elif function_call.data == "end":
+            end_mess = "До новых встреч!"
+            bot.send_message(function_call.message.chat.id, end_mess)
+            bot.answer_callback_query(function_call.id)
 
 
 from parser import parser_vk_id
